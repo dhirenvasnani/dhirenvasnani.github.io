a195995f99bb8b19d0247b890def86988c4110a7
DhirenV <87919051+dhirenvasnani@users.noreply.github.com>
2023-02-09 12:40:18 +0530

    .

--- myscript.js
+++ myscript.js
@@ -1,4 +1,14 @@
 document.addEventListener("contextmenu", function(event) {
     event.preventDefault();
 });
+$(document).ready(function() {
+    $("a.nav-link").click(function(event) {
+      event.preventDefault();
+      var section = $(this).attr("href");
+      $("html, body").animate({
+        scrollTop: $(section).offset().top
+      }, 500);
+    });
+  });
+
 
