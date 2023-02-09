8ef184c72b1eccded1c515d2bd03a5a2e23f4195
DhirenV <87919051+dhirenvasnani@users.noreply.github.com>
2023-02-09 12:45:51 +0530

    .

--- myscript.js
+++ myscript.js
@@ -1,14 +1,5 @@
 document.addEventListener("contextmenu", function(event) {
     event.preventDefault();
 });
-$(document).ready(function() {
-    $("a.nav-link").click(function(event) {
-      event.preventDefault();
-      var section = $(this).attr("href");
-      $("html, body").animate({
-        scrollTop: $(section).offset().top
-      }, 500);
-    });
-  });
 
 
