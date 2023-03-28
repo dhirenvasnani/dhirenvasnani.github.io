55521e6b28e1a1f08e5a7a8f96f67cc02e7ad755
DhirenV <87919051+dhirenvasnani@users.noreply.github.com>
2023-03-29 00:31:04 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,29 +1,9 @@
+// Add event listener to prevent right-click menu from appearing
 document.addEventListener("contextmenu", function(event) {
     event.preventDefault();
 });
 
+// Add event listener to prevent drag and drop action
 document.addEventListener("dragstart", function(event) {
     event.preventDefault();
-});
-
-$(document).ready(function() {
-  $("a.nav-link").click(function(event) {
-    event.preventDefault();
-    var section = $(this).attr("href");
-    $("html, body").animate({
-      scrollTop: $(section).offset().top
-    }, 500, function() {
-      console.log("Scroll animation completed.");
-    });
-  });
-
-  $('.logo').click(function(e) {
-    e.preventDefault();
-    console.log("Logo clicked.");
-    $('html, body').animate({
-      scrollTop: 0
-    }, 1000, function() {
-      console.log("Scroll to top animation completed.");
-    });
-  });
-});
+}); 
