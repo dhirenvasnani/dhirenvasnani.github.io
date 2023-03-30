e9e70525ec6c2386020d9a0e6d8ab91018d38028
DhirenV <87919051+dhirenvasnani@users.noreply.github.com>
2023-03-31 00:30:29 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -8,4 +8,19 @@ document.addEventListener("dragstart", function(event) {
     event.preventDefault();
 }); 
 
-//Done
+$(document).ready(function() {
+  // Show button when user scrolls down a certain amount
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
+
+  // Add smooth scrolling animation when button is clicked
+  $('#back-to-top').click(function() {
+    $('html, body').animate({scrollTop: 0}, 800);
+    return false;
+  });
+});
