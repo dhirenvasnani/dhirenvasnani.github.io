b5cdaf59528ccfafaf0f2c95ca6ea8ed3979e4a8
DhirenV <87919051+dhirenvasnani@users.noreply.github.com>
2023-05-04 18:42:42 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,26 +1,26 @@
 // Add event listener to prevent right-click menu from appearing
-document.addEventListener("contextmenu", function(event) {
-    event.preventDefault();
+document.addEventListener("contextmenu", function (event) {
+ event.preventDefault();
 });
 
 // Add event listener to prevent drag and drop action
-document.addEventListener("dragstart", function(event) {
-    event.preventDefault();
-}); 
+document.addEventListener("dragstart", function (event) {
+ event.preventDefault();
+});
 
-$(document).ready(function() {
-  // Show button when user scrolls down a certain amount
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('#back-to-top').fadeIn();
-    } else {
-      $('#back-to-top').fadeOut();
-    }
-  });
+$(document).ready(function () {
+ // Show button when user scrolls down a certain amount
+ $(window).scroll(function () {
+  if ($(this).scrollTop() > 100) {
+   $("#back-to-top").fadeIn();
+  } else {
+   $("#back-to-top").fadeOut();
+  }
+ });
 
-  // Add smooth scrolling animation when button is clicked
-  $('#back-to-top').click(function() {
-    $('html, body').animate({scrollTop: 0}, 800);
-    return false;
-  });
+ // Add smooth scrolling animation when button is clicked
+ $("#back-to-top").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, 800);
+  return false;
+ });
 });
