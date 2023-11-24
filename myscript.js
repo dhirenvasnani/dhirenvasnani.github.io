f1b5177b58da6c9e6e559435a8a8047e1a38a974
DhirenV <87919051+dhirenvasnani@users.noreply.github.com>
2023-11-24 18:18:48 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,19 +1,3 @@
-// Wrap the JavaScript code in a window load event
-window.addEventListener('resize', setZoom);
-setZoom();
-
-function setZoom() {
-    const screenWidth = window.innerWidth;
-    let zoomLevel = 1; // Default zoom level
-
-    if (screenWidth > 1500) {
-        zoomLevel = screenWidth / 1000;
-        zoomLevel = zoomLevel - 0.7; 
-        document.body.style.zoom = zoomLevel;
-        console.log("Screen Zoom Set to: " + zoomLevel);
-}
-}
-
 // Prevent right-click menu from appearing
 document.addEventListener("contextmenu", function(event) {
     event.preventDefault();
